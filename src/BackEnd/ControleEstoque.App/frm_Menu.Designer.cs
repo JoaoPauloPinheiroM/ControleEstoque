101495fab9b106292db9b8a3785e4d8ec1d8b195
JoaoPauloPinheiroM <joaopaulopinheiro59675@gmail.com>
2025-08-16 17:34:57 -0300

    feat: Cria subtelas para movimentação

--- src/BackEnd/ControleEstoque.App/frm_Menu.Designer.cs
+++ src/BackEnd/ControleEstoque.App/frm_Menu.Designer.cs
@@ -30,38 +30,38 @@
         {
             tabControl1 = new TabControl();
             tbp_Estoque = new TabPage();
+            button2 = new Button();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            textBox8 = new TextBox();
+            textBox7 = new TextBox();
+            textBox6 = new TextBox();
+            textBox5 = new TextBox();
+            dataGridView1 = new DataGridView();
             tbp_Movimentos = new TabPage();
             tbp_Transferencias = new TabPage();
             tbp_Historico = new TabPage();
             tbp_Cadastros = new TabPage();
             tbp_Relatorios = new TabPage();
-            dataGridView1 = new DataGridView();
+            tabControl2 = new TabControl();
+            tbp_EntreProdutos = new TabPage();
+            tbp_EntreDep = new TabPage();
             dataGridView2 = new DataGridView();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            button1 = new Button();
-            textBox3 = new TextBox();
-            textBox4 = new TextBox();
-            checkBox1 = new CheckBox();
-            checkBox2 = new CheckBox();
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            textBox5 = new TextBox();
-            textBox6 = new TextBox();
-            textBox7 = new TextBox();
-            textBox8 = new TextBox();
-            label5 = new Label();
-            label6 = new Label();
-            label7 = new Label();
-            label8 = new Label();
-            button2 = new Button();
+            dataGridView3 = new DataGridView();
+            dataGridView4 = new DataGridView();
             tabControl1.SuspendLayout();
             tbp_Estoque.SuspendLayout();
-            tbp_Movimentos.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            tbp_Movimentos.SuspendLayout();
+            tbp_Transferencias.SuspendLayout();
+            tabControl2.SuspendLayout();
+            tbp_EntreProdutos.SuspendLayout();
+            tbp_EntreDep.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView4).BeginInit();
             SuspendLayout();
             // 
             // tabControl1
@@ -98,20 +98,90 @@
             tbp_Estoque.Text = "Posição Estoque";
             tbp_Estoque.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            button2.Location = new Point(708, 6);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 9;
+            button2.Text = "Mostrar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(300, 12);
+            label8.Name = "label8";
+            label8.Size = new Size(57, 15);
+            label8.TabIndex = 8;
+            label8.Text = "Depósito:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(161, 12);
+            label7.Name = "label7";
+            label7.Size = new Size(51, 15);
+            label7.TabIndex = 7;
+            label7.Text = "Posição:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(7, 33);
+            label6.Name = "label6";
+            label6.Size = new Size(61, 15);
+            label6.TabIndex = 6;
+            label6.Text = "Descrição:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(7, 9);
+            label5.Name = "label5";
+            label5.Size = new Size(49, 15);
+            label5.TabIndex = 5;
+            label5.Text = "Código:";
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(357, 6);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(64, 23);
+            textBox8.TabIndex = 4;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(216, 6);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(64, 23);
+            textBox7.TabIndex = 3;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(73, 30);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(348, 23);
+            textBox6.TabIndex = 2;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(73, 6);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(82, 23);
+            textBox5.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(7, 59);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(776, 281);
+            dataGridView1.TabIndex = 0;
+            // 
             // tbp_Movimentos
             // 
-            tbp_Movimentos.Controls.Add(label4);
-            tbp_Movimentos.Controls.Add(label3);
-            tbp_Movimentos.Controls.Add(label2);
-            tbp_Movimentos.Controls.Add(label1);
-            tbp_Movimentos.Controls.Add(checkBox2);
-            tbp_Movimentos.Controls.Add(checkBox1);
-            tbp_Movimentos.Controls.Add(textBox4);
-            tbp_Movimentos.Controls.Add(textBox3);
-            tbp_Movimentos.Controls.Add(button1);
-            tbp_Movimentos.Controls.Add(textBox2);
-            tbp_Movimentos.Controls.Add(textBox1);
-            tbp_Movimentos.Controls.Add(dataGridView2);
+            tbp_Movimentos.Controls.Add(dataGridView4);
             tbp_Movimentos.Location = new Point(4, 24);
             tbp_Movimentos.Name = "tbp_Movimentos";
             tbp_Movimentos.Padding = new Padding(3);
@@ -122,6 +192,7 @@
             // 
             // tbp_Transferencias
             // 
+            tbp_Transferencias.Controls.Add(tabControl2);
             tbp_Transferencias.Location = new Point(4, 24);
             tbp_Transferencias.Name = "tbp_Transferencias";
             tbp_Transferencias.Padding = new Padding(3);
@@ -160,190 +231,61 @@
             tbp_Relatorios.Text = "Relatórios";
             tbp_Relatorios.UseVisualStyleBackColor = true;
             // 
-            // dataGridView1
+            // tabControl2
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(7, 59);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(776, 281);
-            dataGridView1.TabIndex = 0;
+            tabControl2.Controls.Add(tbp_EntreProdutos);
+            tabControl2.Controls.Add(tbp_EntreDep);
+            tabControl2.Location = new Point(-1, 0);
+            tabControl2.Name = "tabControl2";
+            tabControl2.SelectedIndex = 0;
+            tabControl2.Size = new Size(792, 427);
+            tabControl2.TabIndex = 1;
+            // 
+            // tbp_EntreProdutos
+            // 
+            tbp_EntreProdutos.Controls.Add(dataGridView2);
+            tbp_EntreProdutos.Location = new Point(4, 24);
+            tbp_EntreProdutos.Name = "tbp_EntreProdutos";
+            tbp_EntreProdutos.Padding = new Padding(3);
+            tbp_EntreProdutos.Size = new Size(784, 399);
+            tbp_EntreProdutos.TabIndex = 0;
+            tbp_EntreProdutos.Text = "Entre Produtos";
+            tbp_EntreProdutos.UseVisualStyleBackColor = true;
+            // 
+            // tbp_EntreDep
+            // 
+            tbp_EntreDep.Controls.Add(dataGridView3);
+            tbp_EntreDep.Location = new Point(4, 24);
+            tbp_EntreDep.Name = "tbp_EntreDep";
+            tbp_EntreDep.Padding = new Padding(3);
+            tbp_EntreDep.Size = new Size(784, 399);
+            tbp_EntreDep.TabIndex = 1;
+            tbp_EntreDep.Text = "Entre Depósitos";
+            tbp_EntreDep.UseVisualStyleBackColor = true;
             // 
             // dataGridView2
             // 
             dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView2.Location = new Point(7, 59);
+            dataGridView2.Location = new Point(4, 59);
             dataGridView2.Name = "dataGridView2";
             dataGridView2.Size = new Size(776, 281);
-            dataGridView2.TabIndex = 0;
+            dataGridView2.TabIndex = 1;
             // 
-            // textBox1
+            // dataGridView3
             // 
-            textBox1.Location = new Point(90, 6);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(80, 23);
-            textBox1.TabIndex = 1;
+            dataGridView3.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView3.Location = new Point(4, 59);
+            dataGridView3.Name = "dataGridView3";
+            dataGridView3.Size = new Size(776, 281);
+            dataGridView3.TabIndex = 1;
             // 
-            // textBox2
+            // dataGridView4
             // 
-            textBox2.Enabled = false;
-            textBox2.Location = new Point(90, 30);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(254, 23);
-            textBox2.TabIndex = 2;
-            // 
-            // button1
-            // 
-            button1.Location = new Point(708, 6);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 3;
-            button1.Text = "Processar";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // textBox3
-            // 
-            textBox3.Location = new Point(649, 6);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(53, 23);
-            textBox3.TabIndex = 5;
-            // 
-            // textBox4
-            // 
-            textBox4.Location = new Point(649, 30);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(53, 23);
-            textBox4.TabIndex = 6;
-            // 
-            // checkBox1
-            // 
-            checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(444, 8);
-            checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(84, 19);
-            checkBox1.TabIndex = 7;
-            checkBox1.Text = "Entre Prod.";
-            checkBox1.UseVisualStyleBackColor = true;
-
-            // 
-            // checkBox2
-            // 
-            checkBox2.AutoSize = true;
-            checkBox2.Location = new Point(444, 30);
-            checkBox2.Name = "checkBox2";
-            checkBox2.Size = new Size(80, 19);
-            checkBox2.TabIndex = 8;
-            checkBox2.Text = "Entre Dep.";
-            checkBox2.UseVisualStyleBackColor = true;
-
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(569, 9);
-            label1.Name = "label1";
-            label1.Size = new Size(62, 15);
-            label1.TabIndex = 9;
-            label1.Text = "Dep. Saída";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(569, 33);
-            label2.Name = "label2";
-            label2.Size = new Size(74, 15);
-            label2.TabIndex = 10;
-            label2.Text = "Dep. Entrada";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(21, 12);
-            label3.Name = "label3";
-            label3.Size = new Size(49, 15);
-            label3.TabIndex = 11;
-            label3.Text = "Código:";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(21, 31);
-            label4.Name = "label4";
-            label4.Size = new Size(61, 15);
-            label4.TabIndex = 12;
-            label4.Text = "Descrição:";
-            // 
-            // textBox5
-            // 
-            textBox5.Location = new Point(73, 6);
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(82, 23);
-            textBox5.TabIndex = 1;
-            // 
-            // textBox6
-            // 
-            textBox6.Location = new Point(73, 30);
-            textBox6.Name = "textBox6";
-            textBox6.Size = new Size(348, 23);
-            textBox6.TabIndex = 2;
-            // 
-            // textBox7
-            // 
-            textBox7.Location = new Point(216, 6);
-            textBox7.Name = "textBox7";
-            textBox7.Size = new Size(64, 23);
-            textBox7.TabIndex = 3;
-            // 
-            // textBox8
-            // 
-            textBox8.Location = new Point(357, 6);
-            textBox8.Name = "textBox8";
-            textBox8.Size = new Size(64, 23);
-            textBox8.TabIndex = 4;
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Location = new Point(7, 9);
-            label5.Name = "label5";
-            label5.Size = new Size(49, 15);
-            label5.TabIndex = 5;
-            label5.Text = "Código:";
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.Location = new Point(7, 33);
-            label6.Name = "label6";
-            label6.Size = new Size(61, 15);
-            label6.TabIndex = 6;
-            label6.Text = "Descrição:";
-            // 
-            // label7
-            // 
-            label7.AutoSize = true;
-            label7.Location = new Point(161, 12);
-            label7.Name = "label7";
-            label7.Size = new Size(51, 15);
-            label7.TabIndex = 7;
-            label7.Text = "Posição:";
-            // 
-            // label8
-            // 
-            label8.AutoSize = true;
-            label8.Location = new Point(300, 12);
-            label8.Name = "label8";
-            label8.Size = new Size(57, 15);
-            label8.TabIndex = 8;
-            label8.Text = "Depósito:";
-            // 
-            // button2
-            // 
-            button2.Location = new Point(708, 6);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 9;
-            button2.Text = "Mostrar";
-            button2.UseVisualStyleBackColor = true;
+            dataGridView4.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView4.Location = new Point(7, 73);
+            dataGridView4.Name = "dataGridView4";
+            dataGridView4.Size = new Size(776, 281);
+            dataGridView4.TabIndex = 1;
             // 
             // frm_Menu
             // 
@@ -356,10 +298,15 @@
             tabControl1.ResumeLayout(false);
             tbp_Estoque.ResumeLayout(false);
             tbp_Estoque.PerformLayout();
-            tbp_Movimentos.ResumeLayout(false);
-            tbp_Movimentos.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            tbp_Movimentos.ResumeLayout(false);
+            tbp_Transferencias.ResumeLayout(false);
+            tabControl2.ResumeLayout(false);
+            tbp_EntreProdutos.ResumeLayout(false);
+            tbp_EntreDep.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView4).EndInit();
             ResumeLayout(false);
         }
 
@@ -373,26 +320,20 @@
         private TabPage tbp_Cadastros;
         private TabPage tbp_Relatorios;
         private DataGridView dataGridView1;
-        private TextBox textBox2;
-        private TextBox textBox1;
-        private DataGridView dataGridView2;
-        private Label label2;
-        private Label label1;
-        private CheckBox checkBox2;
-        private CheckBox checkBox1;
-        private TextBox textBox4;
-        private TextBox textBox3;
-        private Button button1;
-        private Label label3;
         private TextBox textBox8;
         private TextBox textBox7;
         private TextBox textBox6;
         private TextBox textBox5;
-        private Label label4;
         private Button button2;
         private Label label8;
         private Label label7;
         private Label label6;
         private Label label5;
+        private TabControl tabControl2;
+        private TabPage tbp_EntreProdutos;
+        private TabPage tbp_EntreDep;
+        private DataGridView dataGridView4;
+        private DataGridView dataGridView2;
+        private DataGridView dataGridView3;
     }
 }
